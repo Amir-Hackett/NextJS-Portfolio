890e5edc1dee8016883b5823058013285d20856b
Amir <amir.hackett@gmail.com>
2024-01-27 12:41:28 -0500

    made projects interactive and linked to another page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c79e44947d7b1c0d"; }
+/******/ 		__webpack_require__.h = function() { return "77f8f6198b25c72a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
