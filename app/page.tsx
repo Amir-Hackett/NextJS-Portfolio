2c39391856765069479118b6f3f0697c555ce0e1
Amir <amir.hackett@gmail.com>
2024-01-27 12:31:01 -0500

    fixed title

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,11 @@ export default async function Home() {
     <div className="max-w-5xl mx-auto py-20">
 
       <h1 className="text-7xl font-extrabold">
-          Hello I&apos;m 
+        Hello I&apos;m{" "}
         <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
-          Amir
-        </span>
-          !
+        Amir
+        </span>  
+        !
       </h1>
 
       <p className="mt-3 text-xl text-gray-600">
