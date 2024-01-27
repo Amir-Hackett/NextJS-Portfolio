5fe44b1cccc03728adca1fc68ffd0aca7024e9b6
Amir <amir.hackett@gmail.com>
2024-01-27 08:02:48 -0500

    embedded sanity into nextjs

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("a63537cd3fb3d267")
+/******/ 		__webpack_require__.h = () => ("5c4ea75a10b54564")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/admin/[[...index]]/page.tsx
+++ app/admin/[[...index]]/page.tsx
@@ -2,7 +2,6 @@
 import config from "@/sanity.config"
 import { NextStudio } from "next-sanity/studio"
 
-
 export default function AdminPage() {
     return <NextStudio config={config} />
 }
